d43862681eb5dc327632c5691eabc79e1c0904c2
BartoszC <Bartosz.Ciesielski1997@gmail.com>
2021-04-21 20:05:48 +0200

    Add datalake connection

---
+++ Python/script-dl.py
@@ -0,0 +1,38 @@
+import pyodbc
+import pandas as pd
+import io
+import sys
+from azure.storage.filedatalake import DataLakeServiceClient, DelimitedTextDialect
+
+
+storage_account_name = "mgrhdsantanderdl2"
+storage_account_key = "****"
+container_name = "khd-datalake"
+directory_name = "tpc-h"
+service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+        "https", storage_account_name), credential=storage_account_key)
+
+file_system_client = service_client.get_file_system_client(file_system=container_name)
+dir_client = file_system_client.get_directory_client(directory_name)
+dir_client.create_directory()
+file_client = dir_client.get_file_client("nation.csv")
+
+csv_stream = io.BytesIO()
+file_client.download_file().readinto(csv_stream)
+csv_stream.seek(0)
+df5 = pd.read_csv(csv_stream, delimiter='|', header='infer')
+df5.where(df5['N_NAME'] == 'BRAZIL', inplace = True)
+excel_stream = io.BytesIO()
+
+dir_client_save = file_system_client.get_directory_client("test-python-output")
+dir_client_save.create_directory()
+file_client_save = dir_client_save.create_file("Brazil_description.xlsx")
+
+
+with pd.ExcelWriter(excel_stream, engine = 'xlsxwriter') as writer:
+    df5.to_excel(writer, sheet_name = "Sheet1", index = False)
+    writer.save()
+
+file_client_save.append_data(data=excel_stream.getvalue(), offset=0, length=len(excel_stream.getvalue()))
+file_client_save.flush_data(len(excel_stream.getvalue()))
+
--- Python/script-pyobdc.py
+++ Python/script-pyobdc.py
@@ -1,19 +1,46 @@
 import pyodbc
 import pandas as pd
-
-server = '####'
-database = '####'
-username = '####'
-password = '####'
+import pandas as pd
+import io
+import sys
+from azure.storage.filedatalake import DataLakeServiceClient
+server = 'mgrhd-santander-khd.sql.azuresynapse.net'
+database = 'stdmgrkhd'
+username = 'sqladmin'
+password = '****'
 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 print(pd.read_sql_query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE';", conn))
 
+delta = -90
 
-QUERY = "SELECT L_PARTKEY, L_SUPPKEY, L_LINENUMBER, L_EXTENDEDPRICE, L_DISCOUNT, L_TAX, L_RETURNFLAG, L_LINESTATUS, L_SHIPDATE, L_COMMITDATE, L_RECEIPTDATE, L_SHIPINSTRUCT, L_SHIPMODE, L_COMMENT FROM LINEITEM "
+QUERY = """SELECT L_RETURNFLAG, L_LINESTATUS, SUM(L_QUANTITY) AS SUM_QTY, SUM(L_EXTENDEDPRICE) AS SUM_BASE_PRICE, SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)) AS SUM_DISC_PRICE, 
+SUM(L_EXTENDEDPRICE*(1-L_DISCOUNT)*(1-L_TAX)) AS SUM_CHARGE, AVG(L_QUANTITY) AS AVG_QTY, AVG(L_EXTENDEDPRICE) AS AVG_PRICE, AVG(L_DISCOUNT) AS AVG_DISC, COUNT(*) AS COUNT_ORDER
+FROM LINEITEM WHERE L_SHIPDATE <= dateadd(dd, """ + str(delta) + """, cast('1998-12-01' as datetime)) GROUP BY L_RETURNFLAG, L_LINESTATUS ORDER BY L_RETURNFLAG, L_LINESTATUS"""
 
-print(pd.read_sql(QUERY, con=conn))
+df5 = pd.read_sql(QUERY, con=conn)
 
-conn.close()
+
+
+storage_account_name = "mgrhdsantanderdl2"
+storage_account_key = "****"
+container_name = "khd-datalake"
+directory_name = "test-python-output"
+service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+        "https", storage_account_name), credential=storage_account_key)
+
+file_system_client = service_client.get_file_system_client(file_system=container_name)
+dir_client = file_system_client.get_directory_client(directory_name)
+dir_client.create_directory()
+file_client = dir_client.create_file("sampledata.xlsx")
+
+excel_stream = io.BytesIO()
+
+with pd.ExcelWriter(excel_stream, engine = 'xlsxwriter') as writer:
+    df5.to_excel(writer, sheet_name = "Sheet1", index = False)
+    writer.save()
+
+file_client.append_data(data=excel_stream.getvalue(), offset=0, length=len(excel_stream.getvalue()))
+file_client.flush_data(len(excel_stream.getvalue()))
 
