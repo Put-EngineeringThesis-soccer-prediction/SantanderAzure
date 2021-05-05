8106546ca3a3c9e7835399e23ce0db5077c39105
BartoszC <Bartosz.Ciesielski1997@gmail.com>
2021-05-05 19:38:02 +0200

    Add master python script

---
+++ Python/test-python-api.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from azure.common.client_factory import get_client_from_auth_file
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+from azure.mgmt.containerinstance.models import (ContainerGroup,
+                                                 Container,
+                                                 ContainerGroupNetworkProtocol,
+                                                 ContainerGroupRestartPolicy,
+                                                 ContainerPort,
+                                                 EnvironmentVariable,
+                                                 IpAddress,
+                                                 Port,
+                                                 ResourceRequests,
+                                                 ResourceRequirements,
+                                                 OperatingSystemTypes)
+
+# Retrieve the IDs and secret to use with ClientSecretCredential
+arguments = sys.argv[1:]
+script_to_run = arguments[0]
+
+os.system(f"py {script_to_run}")
+
+auth_file_path = os.getenv('AZURE_AUTH_LOCATION', None)
+print(auth_file_path)
+if auth_file_path is not None:
+    print("Authenticating with Azure using credentials in file at {0}"
+          .format(auth_file_path))
+
+    aci_client = get_client_from_auth_file(
+        ContainerInstanceManagementClient)
+    res_client = get_client_from_auth_file(ResourceManagementClient)
+else:
+    print("\nFailed to authenticate to Azure. Have you set the")
+
+aci_client.container_groups.delete("test-docker", "mycontainer-mgrstd")
